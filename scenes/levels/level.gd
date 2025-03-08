ca1739fc6d163658352bbc9761e6298713cdc736
JiaMing148 <1485636298@qq.com>
2025-03-08 17:52:03 +0800

    Rigidboady2d & Camera & 子弹与手雷移动 up

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var laser_scene = preload("res://scenes/projectiles/laser.tscn")
+var grenade = preload("res://scenes/projectiles/grenade.tscn")
+
 func _on_gate_player_entered_gate() -> void:
 	print("on player entered gate")
 
@@ -8,9 +11,16 @@ func _on_gate_player_exited_gate() -> void:
 	print("on player exited gate")
 
 
-func _on_player_player_shoot_laser() -> void:
-	print("on player shoot laser")
+func _on_player_player_shoot_laser(pos, dir) -> void:
+	var laser = laser_scene.instantiate() as Area2D
+	laser.position = pos
+	laser.direction = dir
+	laser.rotation_degrees = rad_to_deg(dir.angle())
+	$Projectiles.add_child(laser)
 
-
-func _on_player_player_shooot_grenade() -> void:
-	print("on player shoot grenade")
+func _on_player_player_shooot_grenade(pos, dir) -> void:
+	var grenade = grenade.instantiate() as RigidBody2D
+	grenade.position = pos
+	grenade.linear_velocity = dir * grenade.speed
+	$Projectiles.add_child(grenade)
+	
--- scenes/players/player.gd
+++ scenes/players/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal player_shoot_laser
-signal player_shooot_grenade
+signal player_shoot_laser(pos: Vector2, dir: Vector2)
+signal player_shooot_grenade(pos: Vector2, dir: Vector2)
 
 var speed: float = 800
 var can_laser: bool = true
@@ -13,14 +13,21 @@ func _process(delta: float) -> void:
 	velocity = direction * speed
 	move_and_slide()
 	
+	var mouse_position = get_global_mouse_position();
+	look_at(mouse_position)
+	
 	if can_laser and Input.is_action_pressed("primary action"):
-		player_shoot_laser.emit()
 		can_laser = false
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers.pick_random()
+		var look_direction = (mouse_position - position).normalized()
+		player_shoot_laser.emit(selected_laser.global_position, look_direction)
 		$LaserTimer.start()
 		
 	if can_grenade and Input.is_action_just_pressed("secondary action"):
-		player_shooot_grenade.emit()
 		can_grenade = false
+		var look_direction = (mouse_position - position).normalized()
+		player_shooot_grenade.emit($GrenadeStartPositions.global_position, look_direction)
 		$GrenadeTimer.start()
 
 
